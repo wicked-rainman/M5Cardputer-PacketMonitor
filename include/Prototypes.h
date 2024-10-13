81770491e5de1120ed2ddc91d20a24007f6b7ce0
wicked-rainman <wicked.rainman@protonmail.ch>
2024-10-13 13:41:18 +0100

    Text formatting

--- include/Prototypes.h
+++ include/Prototypes.h
@@ -1,22 +1,22 @@
 #ifndef PROTOTYPES
     #define PROTOTYPES
-    void BatteryStatus(void *);
-    void ChannelHop(void *);
-    void CharReplace(char *, char, char);
-    void DeviceAdd(void *);
-    void DrawCircle(int32_t, int32_t, int32_t,int);
-    void DrawRect(int32_t,int32_t,int32_t,int32_t,int);
-    void DumpDevices();
-    void DumpFile(char *, bool);
-    void DumpNetworks();
-    void GpsReader(void *);
-    void KeyboardBranch(void *);
-    char *LookupOui(char *TargetMac);
-    void NetworkScan(void *);
-    bool RollingMac(uint8_t);
-    void RollToFix(void *);
-    void ScreenPrint(char *, uint8_t,uint8_t, uint8_t,uint16_t,uint16_t);
-    int StoreFind(uint8_t *);
-    bool StorePut(uint8_t *, char *, int32_t);
-    void WiFiPacketHandler(void*, wifi_promiscuous_pkt_type_t type);
+    void    BatteryStatus(void *);
+    void    ChannelHop(void *);
+    void    CharReplace(char *, char, char);
+    void    DeviceAdd(void *);
+    void    DrawCircle(int32_t, int32_t, int32_t,int);
+    void    DrawRect(int32_t,int32_t,int32_t,int32_t,int);
+    void    DumpDevices();
+    void    DumpFile(char *, bool);
+    void    DumpNetworks();
+    void    GpsReader(void *);
+    void    KeyboardBranch(void *);
+    char*   LookupOui(char *TargetMac);
+    void    NetworkScan(void *);
+    bool    RollingMac(uint8_t);
+    void    RollToFix(void *);
+    void    ScreenPrint(char *, uint8_t,uint8_t, uint8_t,uint16_t,uint16_t);
+    int     StoreFind(uint8_t *);
+    bool    StorePut(uint8_t *, char *, int32_t);
+    void    WiFiPacketHandler(void*, wifi_promiscuous_pkt_type_t type);
 #endif
